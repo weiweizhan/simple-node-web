99c9c9d14c4bf158410cc6837bd0d406e921bc69
weiwei.zhan <weiwei.zhan@dianping.com>
2016-05-01 19:04:00 +0800

    simplify dependencies

--- app.js
+++ app.js
@@ -4,20 +4,15 @@ const koa = require('koa');
 const co = require('co');
 const app = koa();
 
-app.use(function *(){
-    this.body = 'Hello World';
+
+co(function* () {
+
+    app.use(function *() {
+        this.body = 'hello';
+    });
+
+    app.listen(3000);
+}).catch((error) => {
+    console.log(error.stack);
+    //process.exit(1);
 });
-
-app.listen(3000);
-
-//co(function* () {
-//
-//    this.body = 'hello';
-//
-//
-//    app.listen();
-//}).catch((e) => {
-//    console.log(e.stack);
-//    app.context.logger.error(e.stack);
-//    process.exit(1);
-//});
